d0e9baeccaccdd22b7a6035f79598b3ac8b9c30c
Robert Adam <dev@robert-adam.de>
2021-05-19 11:50:47 +0200

    Implement TensorDecomposition class

---
+++ src/terms/TensorDecomposition.cpp
@@ -0,0 +1,111 @@
+#include "terms/TensorDecomposition.hpp"
+#include "terms/Index.hpp"
+#include "terms/IndexSpace.hpp"
+
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <unordered_map>
+
+namespace Contractor::Terms {
+
+TensorDecomposition::TensorDecomposition(const TensorDecomposition::substitution_list_t &substitutions)
+	: m_substutions(substitutions) {
+}
+
+TensorDecomposition::TensorDecomposition(TensorDecomposition::substitution_list_t &&substitutions)
+	: m_substutions(substitutions) {
+}
+
+GeneralTerm makeIndicesUnique(const GeneralTerm &substitution, const Term &term) {
+	// Construct a map holding the highest existing index ID in the given term
+	std::unordered_map< IndexSpace, Index::id_t > indexMap;
+
+	for (const Tensor &currentTensor : term.getTensors()) {
+		for (const Index &currentIndex : currentTensor.getIndices()) {
+			assert(currentIndex.getID() < std::numeric_limits< Index::id_t >::max() - 1);
+
+			indexMap[currentIndex.getSpace()] = std::max(indexMap[currentIndex.getSpace()], currentIndex.getID() + 1);
+		}
+	}
+
+	GeneralTerm copy(substitution);
+	for (Tensor &currentTensor : copy.accessTensors()) {
+		for (std::size_t i = 0; i < currentTensor.getIndices().size(); i++) {
+			const Index &index = currentTensor.getIndices()[i];
+
+			const Tensor::index_list_t &indexList = substitution.getResult().getIndices();
+			if (std::find(indexList.begin(), indexList.end(), index) != indexList.end()) {
+				// This is an index of the substitute -> This one we want to keep
+				continue;
+			}
+
+			if (index.getID() < indexMap[index.getSpace()]) {
+				// Replace this index with a version with a higher ID
+				assert(indexMap[index.getSpace()] < std::numeric_limits< Index::id_t >::max() - 1);
+
+				Index replacement(index);
+				replacement.setID(indexMap[replacement.getSpace()]++);
+
+				currentTensor.replaceIndex(index, replacement);
+			} else {
+				// Update highest index stored in map
+				assert(index.getID() < std::numeric_limits< Index::id_t >::max() - 1);
+
+				indexMap[index.getSpace()] = index.getID() + 1;
+			}
+		}
+	}
+
+	return GeneralTerm(copy);
+}
+
+TensorDecomposition::decomposed_terms_t TensorDecomposition::apply(const Term &term) {
+	TensorDecomposition::decomposed_terms_t result;
+
+	for (const GeneralTerm &sub : m_substutions) {
+		// We have to make sure that the indices in our substitution don't collide with further indices
+		// in the given term
+		GeneralTerm currentSubstitution = makeIndicesUnique(sub, term);
+
+		GeneralTerm::tensor_list_t tensorList;
+		for (const Tensor &currentTensor : term.getTensors()) {
+			if (currentTensor.refersToSameElement(currentSubstitution.getResult())) {
+				// This is the Tensor we intend to substitute -> Instead of the Tensor itself, push the
+				// current substitution to the list
+				// But before we can do that, we have to make sure that the actual indices in our replacement
+				// term match the indices of the Tensor that we are about to replace (up to this point we know
+				// only that they refer to the same element which does not uniquely define the actual index IDs)
+				for (const std::pair< Index, Index > currentPair :
+					 currentSubstitution.getResult().getIndexMapping(currentTensor)) {
+					if (currentPair.first == currentPair.second) {
+						// Indices are the same already -> nothing to replace
+						continue;
+					}
+
+					for (Tensor &subTensor : currentSubstitution.accessTensors()) {
+						subTensor.replaceIndex(currentPair.first, currentPair.second);
+					}
+				}
+
+				tensorList.reserve(tensorList.size() + currentSubstitution.size());
+				// Append all Tensors from currentSubstitution to the end of tensorList
+				tensorList.insert(tensorList.end(), currentSubstitution.getTensors().begin(),
+								  currentSubstitution.getTensors().end());
+			} else {
+				tensorList.push_back(currentTensor);
+			}
+		}
+
+		result.push_back(GeneralTerm(term.getResult(), term.getPrefactor() * currentSubstitution.getPrefactor(),
+									 std::move(tensorList)));
+	}
+
+	return result;
+}
+
+const TensorDecomposition::substitution_list_t &TensorDecomposition::getSubstitutions() const {
+	return m_substutions;
+}
+
+}; // namespace Contractor::Terms
---
+++ tests/terms/TensorDecompositionTest.cpp
@@ -0,0 +1,153 @@
+#include "terms/TensorDecomposition.hpp"
+#include "terms/GeneralTerm.hpp"
+#include "terms/Index.hpp"
+#include "terms/Tensor.hpp"
+
+#include <gtest/gtest.h>
+
+namespace ct = Contractor::Terms;
+
+TEST(TensorDecompositionTest, getter) {
+	ct::TensorDecomposition::substitution_list_t substitutions = { ct::GeneralTerm(ct::Tensor("T", {}), 1.0,
+																				   { ct::Tensor("H", {}) }) };
+
+	ct::TensorDecomposition decomposition(substitutions);
+
+	ASSERT_EQ(decomposition.getSubstitutions(), substitutions);
+}
+
+TEST(TensorDecompositionTest, apply) {
+	ct::Index i(ct::IndexSpace(0), 0, ct::Index::Type::Creator);
+	ct::Index j(ct::IndexSpace(0), 1, ct::Index::Type::Creator);
+	ct::Index k(ct::IndexSpace(0), 2, ct::Index::Type::Creator);
+
+	ct::Index a(ct::IndexSpace(1), 0, ct::Index::Type::Creator);
+	ct::Index b(ct::IndexSpace(1), 1, ct::Index::Type::Creator);
+	ct::Index c(ct::IndexSpace(1), 2, ct::Index::Type::Creator);
+
+	ct::Tensor result("Res", {});
+	constexpr ct::Term::factor_t originalFactor = 1.0;
+
+	ct::Tensor substitute("H", { ct::Index(i), ct::Index(j), ct::Index(a), ct::Index(b) });
+	ct::Tensor secondTensor("T", { ct::Index(i), ct::Index(k), ct::Index(c) });
+
+	// Res = H[ij,ab] T[ik,c]
+	ct::GeneralTerm originalTerm(result, originalFactor, { ct::Tensor(substitute), ct::Tensor(secondTensor) });
+
+	{
+		// Remove term
+		constexpr ct::Term::factor_t decompositionFactor           = 2.0;
+		ct::TensorDecomposition::substitution_list_t substitutions = { ct::GeneralTerm(substitute, decompositionFactor,
+																					   {}) };
+
+		ct::TensorDecomposition decomposition(substitutions);
+
+		ct::TensorDecomposition::decomposed_terms_t decomposed = decomposition.apply(originalTerm);
+
+		ct::GeneralTerm expected(result, originalFactor * decompositionFactor, { ct::Tensor(secondTensor) });
+
+		// Res = 2 T[ik,c]
+		ASSERT_EQ(decomposed.size(), 1);
+		ASSERT_EQ(decomposed[0], expected);
+	}
+	{
+		// Replace with single term
+		ct::Tensor substituted("S", {});
+		constexpr ct::Term::factor_t decompositionFactor           = 2.0;
+		ct::TensorDecomposition::substitution_list_t substitutions = { ct::GeneralTerm(substitute, decompositionFactor,
+																					   { ct::Tensor(substituted) }) };
+
+		ct::TensorDecomposition decomposition(substitutions);
+
+		ct::TensorDecomposition::decomposed_terms_t decomposed = decomposition.apply(originalTerm);
+
+		// Res = 2 * S[] T[ik,c]
+		ct::GeneralTerm expected(result, originalFactor * decompositionFactor,
+								 { ct::Tensor(substituted), ct::Tensor(secondTensor) });
+
+		ASSERT_EQ(decomposed.size(), 1);
+		ASSERT_EQ(decomposed[0], expected);
+	}
+	{
+		// Replace with multiple terms
+		ct::Tensor substituted1("S1", {});
+		ct::Tensor substituted2("S2", {});
+		constexpr ct::Term::factor_t factor1                       = 2.0;
+		constexpr ct::Term::factor_t factor2                       = -1;
+		ct::TensorDecomposition::substitution_list_t substitutions = {
+			ct::GeneralTerm(substitute, factor1, { ct::Tensor(substituted1) }),
+			ct::GeneralTerm(substitute, factor2, { ct::Tensor(substituted2) })
+		};
+
+		ct::TensorDecomposition decomposition(substitutions);
+
+		ct::TensorDecomposition::decomposed_terms_t decomposed = decomposition.apply(originalTerm);
+
+		// Res = 2 * S1[] T[ik,c] - 1 * S2[] T[ik,c]
+		ct::GeneralTerm expected1(result, originalFactor * factor1,
+								  { ct::Tensor(substituted1), ct::Tensor(secondTensor) });
+		ct::GeneralTerm expected2(result, originalFactor * factor2,
+								  { ct::Tensor(substituted2), ct::Tensor(secondTensor) });
+
+		ASSERT_EQ(decomposed.size(), 2);
+		ASSERT_EQ(decomposed[0], expected1);
+		ASSERT_EQ(decomposed[1], expected2);
+	}
+
+	{
+		// Replace with single term that collides with indices in the second Tensor, while having common indices with
+		// the substituted Tensor
+		ct::Tensor substituted("S", { ct::Index(i), ct::Index(c) });
+		constexpr ct::Term::factor_t decompositionFactor           = 2.0;
+		ct::TensorDecomposition::substitution_list_t substitutions = { ct::GeneralTerm(substitute, decompositionFactor,
+																					   { ct::Tensor(substituted) }) };
+
+		ct::TensorDecomposition decomposition(substitutions);
+
+		ct::TensorDecomposition::decomposed_terms_t decomposed = decomposition.apply(originalTerm);
+
+		// Res = 2 * S[i, d] T[ik,c]
+		// The substitution will use indices that have index IDs higher than any other indices in the remaining term.
+		// That is except for indices that the substitution shares with the substituted Tensor. These are conserved
+		ct::Index d(c);
+		d.setID(c.getID() + 1);
+		ct::GeneralTerm expected(result, originalFactor * decompositionFactor,
+								 { ct::Tensor("S", { ct::Index(i), ct::Index(d) }), ct::Tensor(secondTensor) });
+
+		ASSERT_EQ(decomposed.size(), 1);
+		ASSERT_EQ(decomposed[0], expected);
+	}
+	{
+		// This time the decomposition is specified in terms of a Tensor that refers to the same element as the substitute in
+		// the original Term. That means that the corresponding indices of the decomposition term have to be mapped to the actual
+		// indices in the Term in order to make sure that the result contains the correct indices.
+		ct::Index altI(i.getSpace(), 20, i.getType(), i.getSpin());
+		ct::Index altJ(j.getSpace(), 21, j.getType(), j.getSpin());
+		ct::Index altA(a.getSpace(), 20, a.getType(), a.getSpin());
+		ct::Index altB(b.getSpace(), 21, b.getType(), b.getSpin());
+		ct::Tensor altSubstitute(substitute.getName(),
+								 { ct::Index(altI), ct::Index(altJ), ct::Index(altA), ct::Index(altB) });
+
+		ASSERT_TRUE(altSubstitute.refersToSameElement(substitute));
+
+		ct::Tensor substituted("S", { ct::Index(altI), ct::Index(c) });
+		constexpr ct::Term::factor_t decompositionFactor           = -3;
+		ct::TensorDecomposition::substitution_list_t substitutions = { ct::GeneralTerm(altSubstitute, decompositionFactor,
+																					   { ct::Tensor(substituted) }) };
+
+		ct::TensorDecomposition decomposition(substitutions);
+
+		ct::TensorDecomposition::decomposed_terms_t decomposed = decomposition.apply(originalTerm);
+
+		// Res = -3 * S[i, d] T[ik,c]
+		// The substitution will use indices that have index IDs higher than any other indices in the remaining term.
+		// That is except for indices that the substitution shares with the substituted Tensor. These are conserved
+		ct::Index d(c);
+		d.setID(c.getID() + 1);
+		ct::GeneralTerm expected(result, originalFactor * decompositionFactor,
+								 { ct::Tensor("S", { ct::Index(i), ct::Index(d) }), ct::Tensor(secondTensor) });
+
+		ASSERT_EQ(decomposed.size(), 1);
+		ASSERT_EQ(decomposed[0], expected);
+	}
+}
