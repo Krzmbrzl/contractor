b42770e52613824f189aa6c50b2d122b59ace6a5
Robert Adam <dev@robert-adam.de>
2021-06-02 11:28:27 +0200

    Make Index implicitly copyable

--- include/terms/Index.hpp
+++ include/terms/Index.hpp
@@ -39,7 +39,7 @@ public:
 	constexpr explicit Index(const IndexSpace &space, id_t id, Type type, Spin spin = Spin::None)
 		: m_space(space), m_id(id), m_type(type), m_spin(spin){};
 
-	constexpr explicit Index(const Index &other) = default;
+	constexpr Index(const Index &other) = default;
 	constexpr Index(Index &&other)               = default;
 	Index &operator=(const Index &other) = default;
 	Index &operator=(Index &&other) = default;
