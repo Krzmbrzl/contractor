bcf5ff4cefe61c34e5b001187c9f44e7f5df4759
Robert Adam <dev@robert-adam.de>
2021-06-16 08:45:29 +0200

    Remove outdated function definintion

--- include/processor/Factorizer.hpp
+++ include/processor/Factorizer.hpp
@@ -30,20 +30,6 @@ protected:
 					 std::vector< Terms::BinaryTerm > &factorizedTerms, const Terms::GeneralTerm &term);
 };
 
-/**
- * Computes the optimal factorization into BinaryTerm objects for the given term. The optimal factorization
- * is the one with the least cost where cost equals amount of iterations that need to be performed in the
- * contraction of the individual Tensors.
- *
- * @param term The GeneralTerm to factorize
- * @param resolver The IndexSpaceResolver to use for accessing the index space sizes
- * @param factorizationCost A pointer to a variable in which the cost of the found factorization is to be
- * stored. This may be nullptr if this information is not needed.
- * @returns A list of BinaryTerm objects that represent the optimal factorization.
- */
-//std::vector< Terms::BinaryTerm > factorize(const Terms::GeneralTerm &term, const Utils::IndexSpaceResolver &resolver,
-//										   Terms::ContractionResult::cost_t *factorizationCost = nullptr);
-
 }; // namespace Contractor::Processor
 
 #endif // CONTRACTOR_PROCESSOR_FACTORIZER_HPP_
