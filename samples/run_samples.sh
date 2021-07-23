1f0516cf138092e6aedcae795907a5a9f7c5cb49
Robert Adam <dev@robert-adam.de>
2021-07-23 17:38:35 +0200

    Always also run main application on sample inputs

--- samples/run_samples.sh
+++ samples/run_samples.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-mainDir="$(dirname "$(readlink -f "$0")")"
-program="$mainDir/../build/bin/contractor"
+program="$(find ../build/ -type f \( -iname "contractor" -o -iname "contractor.exe" \) )"
 
-if [ ! -f "$program" ]; then
+if [[ ! -f "$program" ]]; then
 	echo "Unable to find the executable at $program"
 	exit 1
 fi
 
-for dir in $(find "$mainDir" -maxdepth 1 -mindepth 1 -type d); do
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d); do
 	dir_name="$(basename "$dir")"
 
-	for export_file in $(find "$mainDir/$dir_name" -maxdepth 1 -mindepth 1 -type f -iname "*.EXPORT"); do
+	for export_file in $(find "./$dir_name" -maxdepth 1 -mindepth 1 -type f -iname "*.EXPORT"); do
 		out_file="${dir_name}_$(basename $export_file .EXPORT).out"
-		symmetry_file="$(find $mainDir/$dir_name -type f -iname "*.symmetry")"
-		symmetry_file="$(find $mainDir/$dir_name -type f -iname "*.symmetry")"
+		symmetry_file="$(find ./$dir_name -type f -iname "*.symmetry")"
+		symmetry_file="$(find ./$dir_name -type f -iname "*.symmetry")"
 
 		echo "Processing '$export_file' - output goes to '$out_file' ..."
 
-		"$program" --decomposition "$mainDir/density_fitting.decomposition" --symmetry "$symmetry_file" --index-spaces \
-			"$mainDir/index_spaces.json" --gecco-export "$export_file" --restricted-orbitals > "$out_file"
+		"$program" --decomposition "./density_fitting.decomposition" --symmetry "$symmetry_file" --index-spaces \
+			"./index_spaces.json" --gecco-export "$export_file" --restricted-orbitals > "$out_file"
 	done
 done
