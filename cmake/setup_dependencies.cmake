06f04615efd78ba580c946576eb86446dba51256
Robert Adam <dev@robert-adam.de>
2021-10-28 09:45:12 +0200

    Add fallback for older Boost versions
    We require the is_detected type_trait which was only introduced in Boost
    1.67.0. However, since the type_traits library is more or less
    self-contained and does not have big dependencies on other Boost
    libraries, this commit adds a workaround that will simply download the
    Boost type_traits library in version 1.67.0 and add it to the
    include-path for the program. That way the is_detected header should be
    found and be usable, even if the overall Boost version that is used does
    not contain it.

--- cmake/setup_dependencies.cmake
+++ cmake/setup_dependencies.cmake
@@ -4,6 +4,22 @@ include(FetchContent)
 # We only have to specify the libs that are NOT header-only as dependencies
 find_package(Boost REQUIRED COMPONENTS program_options)
 
+if (${Boost_VERSION_STRING} VERSION_LESS "1.67.0")
+	message(STATUS "Detected Boost version is not recent enough to support the is_detected type-trait. Including packaged headers into search path...")
+
+	FetchContent_Declare(
+		boost_type_traits
+		GIT_REPOSITORY https://github.com/boostorg/type_traits.git
+		GIT_TAG boost-1.67.0
+	)
+
+	FetchContent_GetProperties(boost_type_traits)
+	if(NOT boost_type_traits_POPULATED)
+		FetchContent_Populate(boost_type_traits)
+		include_directories(AFTER "${boost_type_traits_SOURCE_DIR}/include/")
+	endif()
+endif()
+
 # nlohmann-JSON
 FetchContent_Declare(
 	json
