bdb87facbf47576593c11a653234cba6efe086d5
Robert Adam <dev@robert-adam.de>
2021-10-28 12:58:16 +0200

    Add option to use a bundled Boost distribution
    This is intended for systems that do not meet the minimal required Boost version
    and for which the workaround introduced in 06f04615efd78ba580c946576eb86446dba51256
    doesn't work as intended (or other problems arise with the older Boost version).

--- README.md
+++ README.md
@@ -6,8 +6,12 @@
 
 - A cpp17 conform compiler
 - cmake v3.15 or newer
-- Boost (headers + program_options)
+- Boost (headers + program_options) - at least v1.67 (though a workaround for earlier versions is integrated in the build system), better v1.70 or newer
 - An active internet connection (only needed for first build)
+
+If you don't have Boost installed on your system or the version installed is not recent enough, you can use the `use-bundled-boost` option when invoking cmake.
+This will download and build a recent Boost automatically when running cmake. In order to make use of this option, execute `git submodule update --init` first
+and then invoke cmake as `cmake -Duse-bundled-boost=ON ..`.
 
 ### Instructions
 
--- cmake/setup_dependencies.cmake
+++ cmake/setup_dependencies.cmake
@@ -1,22 +1,32 @@
 include(FetchContent)
 
+option(use-bundled-boost OFF)
+
 # Boost
 # We only have to specify the libs that are NOT header-only as dependencies
-find_package(Boost REQUIRED COMPONENTS program_options)
+if (use-bundled-boost)
+	if (NOT EXISTS "${CMAKE_SOURCE_DIR}/3rdParty/boost-cmake/CMakeLists.txt")
+		message(FATAL_ERROR "boost-cmake submodule not initialized. Please run git submodule update --init first")
+	endif()
 
-if (${Boost_VERSION_STRING} VERSION_LESS "1.67.0")
-	message(STATUS "Detected Boost version is not recent enough to support the is_detected type-trait. Including packaged headers into search path...")
+	add_subdirectory("${CMAKE_SOURCE_DIR}/3rdParty/boost-cmake/")
+else()
+	find_package(Boost REQUIRED COMPONENTS program_options)
 
-	FetchContent_Declare(
-		boost_type_traits
-		GIT_REPOSITORY https://github.com/boostorg/type_traits.git
-		GIT_TAG boost-1.67.0
-	)
+	if (${Boost_VERSION_STRING} VERSION_LESS "1.67.0")
+		message(STATUS "Detected Boost version is not recent enough to support the is_detected type-trait. Including packaged headers into search path...")
 
-	FetchContent_GetProperties(boost_type_traits)
-	if(NOT boost_type_traits_POPULATED)
-		FetchContent_Populate(boost_type_traits)
-		include_directories(AFTER "${boost_type_traits_SOURCE_DIR}/include/")
+		FetchContent_Declare(
+			boost_type_traits
+			GIT_REPOSITORY https://github.com/boostorg/type_traits.git
+			GIT_TAG boost-1.67.0
+		)
+
+		FetchContent_GetProperties(boost_type_traits)
+		if(NOT boost_type_traits_POPULATED)
+			FetchContent_Populate(boost_type_traits)
+			include_directories(AFTER "${boost_type_traits_SOURCE_DIR}/include/")
+		endif()
 	endif()
 endif()
 
